0cce3e4b2d52437b2395e6792bd25f85ffd338b7
root <elishahezekiah903@gmail.com>
2021-07-27 15:39:06 -0700

    to handle %s and %c

--- _printf.c
+++ _printf.c
@@ -7,5 +7,36 @@
 int _printf(const char *format, ...)
 {
 	/*insert something here*/
+	char *traverse;
+	unsigned int i;
+	char *s;
+
+	/*we start here*/
+	va_list arg;
+
+	va_start(arg, format);
+	for (traverse = format; *traverse != '\0'; traverse++)
+	{
+		while (*traverse != '%')
+		{
+			_putchar(*traverse);
+			traverse++;
+		}
+		traverse++;
+		/*this is step 2*/
+		if (*traverse == 'c')
+		{
+			i = va_arg(arg, int);
+			_putchar(i);
+			break;
+		}
+		else if (*traverse == 's')
+		{
+			s = va_arg(arg, char *);
+			_puts(s);
+			break;
+		}
+	}
+	va_end(arg);
 	return (0);
 }
--- holberton.h
+++ holberton.h
@@ -1,9 +1,12 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#include <stdio.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int _strlen(char *s);
+int _puts(char *s);
 
 #endif
